abb1e7e28391a71ea0805d7526f9b0977ef27431
Theophile VITOUX <tvitoux@f2r8s14.clusters.42paris.fr>
2026-05-21 13:13:54 +0200

    ex09 pas bon - a investiguer

---
+++ d05/d05/ex09/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import People
+
+
+def display(request):
+    try:
+        people = People.objects.filter(
+            homeworld__climate__contains='windy'
+        ).select_related('homeworld').order_by('name')
+        if not people.exists():
+            return HttpResponse(
+                "No data available, please use the following command line before use:<br>"
+                "python manage.py loaddata ex09/ex09_initial_data.json"
+            )
+        return render(request, 'ex09/display.html', {'people': people})
+    except Exception:
+        return HttpResponse(
+            "No data available, please use the following command line before use:<br>"
+            "python manage.py loaddata ex09/ex09_initial_data.json"
+        )
